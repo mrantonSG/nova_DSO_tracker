3255d408680585ace0ab6270e200bf1ee7e0126c
Anton <anton.gutscher@gmail.com>
2026-01-08 22:48:44 +0100

    v4.7.5

--- nova.py
+++ nova.py
@@ -99,7 +99,7 @@ log.setLevel(logging.ERROR)
 
 import re
 
-APP_VERSION = "4.7.0"
+APP_VERSION = "4.7.5"
 
 INSTANCE_PATH = os.environ.get("INSTANCE_PATH") or globals().get("INSTANCE_PATH") or os.path.join(os.getcwd(), "instance")
 os.makedirs(INSTANCE_PATH, exist_ok=True)
